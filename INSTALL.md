b592e7cb9298b12ec25696b5850ff4b96349edb6
Niyaz Akhmetov <niyaz.akhmetov@netcracker.com>
2020-04-26 22:42:25 +0300

    :rocket: init

---
+++ INSTALL.md
@@ -0,0 +1,2 @@
+sudo apt install -y xdotool
+sudo apt install -y sox
---
+++ create_doc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+sleep 1
+xdotool click 1
+sleep 1
+xdotool key ctrl+a
+sleep 1
+xdotool key ctrl+c
+sleep 1
+xdotool key alt+shift+c
+sleep 5
+xdotool key ctrl+v
+sleep 2
+sleep 1
+xdotool click 1
+sleep 1
+xdotool key 'ctrl+a'
+sleep 1
+xdotool key 'ctrl+backslash'
+sleep 1
+xdotool key alt+c
+sleep 1
+xdotool type 'wow()'
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Escape
+sleep 1
+xdotool key alt+f
+sleep 1.5
+xdotool key r
+play -v 0.1 "`dirname $0`"/hollow.ogg
